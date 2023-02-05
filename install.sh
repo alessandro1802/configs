ef12a04d9505aeece86be97343a3819a9b65c15b
Alessandro <oleksandr.yasinskyi@icloud.com>
2023-02-05 15:27:30 +0100

    updated installation script

--- install.sh
+++ install.sh
@@ -3,12 +3,3 @@ dnf remove -y libreoffice*
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install -y flathub com.mattjakeman.ExtensionManager flathub com.visualstudio.code md.obsidian.Obsidian org.libreoffice.LibreOffice org.telegram.desktop flathub com.discordapp.Discord
-
-ln -sr ./vim/.vimrc ~/.vimrc
-ln -sr ./vim/.vim ~/.vim
-
-rm ~/.zshrc
-ln -sr zsh/.zshrc ~/.zshrc
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-ln -sr ./.bin ~/.bin
