1aec1bcdbefbbb6c09836b33c6d5cb90efb9ce8b
Alessandro <vita02051802@gmail.com>
2023-02-12 20:55:47 +0100

    updated installation script

--- install.sh
+++ install.sh
@@ -1,5 +1,21 @@
-dnf install -y gnome-tweaks python3.8 thunderbird vim-X11 zsh util-linux-user xclip
+#!/bin/bash
+
+dnf install -y \
+	gnome-tweaks \
+	python3.8 \
+	thunderbird \
+	vim-X11 \
+	zsh \
+	util-linux-user \
+	xclip \
+	neofetch
 dnf remove -y libreoffice*
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install -y flathub com.mattjakeman.ExtensionManager flathub com.visualstudio.code md.obsidian.Obsidian org.libreoffice.LibreOffice org.telegram.desktop flathub com.discordapp.Discord
+flatpak install -y flathub \
+	com.mattjakeman.ExtensionManager \
+	com.visualstudio.code \
+	md.obsidian.Obsidian \
+	org.libreoffice.LibreOffice \
+	org.telegram.desktop\
+	com.discordapp.Discord
