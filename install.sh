364f0cb949eb221f636b6f7b18b01767b8350af6
Alessandro <oleksandr.yasinskyi@icloud.com>
2023-02-28 19:40:16 +0100

    updated installation script

--- install.sh
+++ install.sh
@@ -26,11 +26,11 @@ sudo flatpak install -y flathub \
 	com.discordapp.Discord \
 
 # Create symlinks to Vim config and files
-ln -srf ./vim/.vimrc ~/.vimrc
-ln -srf ./vim/.vim ~/.vim
+ln -srf ./vim/.vimrc ~/
+ln -srf ./vim/.vim ~/
 
 # Create symlink to zsh config
-ln -srf zsh/.zshrc ~/.zshrc
+ln -srf zsh/.zshrc ~/
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # Make zsh the default shell
@@ -38,7 +38,8 @@ chsh -s $(which zsh)
 
 # Create symlinks to tmux config and files
 git clone https://github.com/gpakosz/.tmux.git ~/.tmux
-# TODO
+ln -sr  ~/.tmux/.tmux.conf ~/
+ln -sr ./.tmux.conf.local ~/
 
 # Create symlink to scripts (already on $PATH in the zsh config)
-ln -sr ./.bin ~/.bin
+ln -sr ./.bin ~/
